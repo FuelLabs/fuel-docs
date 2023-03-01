e28ed9b086a482977b99aa9078dfcba5855f8883
dabit3 <dabit3@gmail.com>
2023-03-01 07:52:52 -0700

    consistent syntax for imports

--- quickstart-example/frontend/src/App.tsx
+++ quickstart-example/frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { bn } from 'fuels'
+import { bn } from 'fuels';
 
 import "./App.css";
 // Import the contract factory -- you can find the name in index.ts.
