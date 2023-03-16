7c70a8f573b1e3cc82cd18bebbafa6c9b4856dc5
sarahschwartz <58856580+sarahschwartz@users.noreply.github.com>
2023-03-16 15:06:44 -0600

    fix test

--- quickstart-example/counter-contract/tests/harness.rs
+++ quickstart-example/counter-contract/tests/harness.rs
@@ -35,10 +35,19 @@ async fn get_contract_instance() -> (MyContract, ContractId) {
 
     (instance, id.into())
 }
-
+// ANCHOR: contract-test
 #[tokio::test]
 async fn can_get_contract_id() {
-    let (_instance, _id) = get_contract_instance().await;
+    let (instance, _id) = get_contract_instance().await;
 
-    // Now you have an instance of your contract you can use to test each function
+    // Increment the counter
+    instance.methods().increment().call().await.unwrap();
+
+    // Get the current value of the counter
+    let result = instance.methods().count().call().await.unwrap();
+
+    // Check that the current value of the counter is 1.
+    // Recall that the initial value of the counter was 0.
+    assert_eq!(result.value, 1);
 }
+// ANCHOR_END: contract-test
